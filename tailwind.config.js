c346daf22a76818303c5ea45938053089008cb67
Brayan Bahamon <desarrollador.junior3@interedes.com.co>
2021-01-12 14:28:42 -0500

    git inicial del proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      container: {
+        center: 'true',
+        padding: '1rem'
+      },
+      colors: {
+        warning: '#FF9F1C',
+        success: '#2EC4B6',
+        danger: '#E71D36',
+      },
+      boxShadow: {
+        top: '0px -5px 15px rgba(0, 0, 0, 0.1)',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
